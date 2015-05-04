ed8e379bfda0c1a35573aff8d84348c21f011c8d
Steve & Courtney Phillips <student@epicodus.com>
2015-05-04 14:00:03 -0700

    Add second table to Database. Add List class and specs for class. Pass all specs.

--- lib/to_do.rb
+++ lib/to_do.rb
@@ -2,10 +2,11 @@
 class To_Do
 @@task_list = []
 
-  attr_reader(:task)
+  attr_reader(:task, :list_id)
 
   define_method(:initialize) do |inputs|
     @task = inputs.fetch(:task)
+    @list_id = inputs.fetch(:list_id)
   end
 
   define_singleton_method(:all) do
@@ -13,13 +14,14 @@ class To_Do
     tasks = []
     returned_tasks.each() do |task|
       description = task.fetch("description")
-      tasks.push(To_Do.new({:task => description}))
+      list_id = task.fetch("list_id").to_i
+      tasks.push(To_Do.new({:task => description, :list_id => list_id}))
     end
     tasks
   end
 
   define_method(:add) do
-     DB.exec("INSERT INTO tasks (description) VALUES ('#{@task}');")
+     DB.exec("INSERT INTO tasks (description, list_id) VALUES ('#{@task}', #{@list_id});")
   end
 
   define_method(:description) do
--- spec/to_do_spec.rb
+++ spec/to_do_spec.rb
@@ -1,41 +1,33 @@
-require('rspec')
-require('to_do')
-require('pg')
-
-DB = PG.connect({:dbname => 'to_do_test'})
-
-RSpec.configure do |config|
-  config.after(:each) do
-    DB.exec("DELETE FROM tasks *;")
-  end
-end
+require 'spec_helper'
 
 describe(To_Do) do
 
   describe('#add') do
     it("adds a task to the array of saved tasks") do
-      test_task = To_Do.new({:task => "learn SQL"})
+      test_task = To_Do.new({:task => "learn SQL", :list_id => 1})
       test_task.add()
       expect(To_Do.all()).to(eq([test_task]))
     end
   end
 
-  # describe('.all') do
-  #   it("returns all data") do
-
-
+  describe("#list_id") do
+    it("lets you read the list ID out") do
+      test_task = To_Do.new({:task => "learn SQL", :list_id => 1})
+      expect(test_task.list_id).to(eq(1))
+    end
+  end
 
   describe('#description') do
     it('reads out the description for chosen task') do
-      task1 = To_Do.new({:task => "Wash the dog."})
+      task1 = To_Do.new({:task => "Wash the dog.", :list_id => 1})
       expect(task1.description()).to(eq("Wash the dog."))
     end
   end
 
   describe("#==") do
     it("is the same task if it has the same description") do
-      task1 = To_Do.new({:task => "learn SQL"})
-      task2 = To_Do.new({:task => "learn SQL"})
+      task1 = To_Do.new({:task => "learn SQL", :list_id => 1})
+      task2 = To_Do.new({:task => "learn SQL", :list_id => 2})
       expect(task1).to(eq(task2))
     end
   end
